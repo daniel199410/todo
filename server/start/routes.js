eda77e364a7d419ecc6dea238204cca71685984a
Daniel Cataño Restrepo <dasniel19910@gmail.com>
2018-06-04 19:19:03 -0500

    Task index + create

--- server/start/routes.js
+++ server/start/routes.js
@@ -1,18 +1,5 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
 const Route = use('Route')
 
 Route.group(() => {   
@@ -23,4 +10,7 @@ Route.group(() => {
     Route.get('projects', 'ProjectController.index');
     Route.patch('projects/:id', 'ProjectController.update').middleware('auth');
     Route.post('projects', 'ProjectController.create').middleware('auth');
+
+    Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth');
+    Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth');
 }).prefix('api');
